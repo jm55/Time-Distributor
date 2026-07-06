4638056c3c153b353a5b87faf846442c427c705f
Miguel Escalona <ejosemiguel70@gmail.com>
2026-07-06 16:49:06 +0800

    Revert "Version correction"

--- Java/src/mains/Driver.java
+++ Java/src/mains/Driver.java
@@ -12,7 +12,7 @@ import files.FileHandler;
 import gui.GUI;
 import compute.Compute;
 
-public class TimeDistributor {
+public class Driver {
 
 	private static GUI g;
 	private static Controller c;
